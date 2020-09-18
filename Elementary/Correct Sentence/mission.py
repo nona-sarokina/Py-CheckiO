114365ee725c491bac20ad959494964eacf93a30
Nona Sarokina <forgakk@gmail.com>
2020-09-18 16:25:54 +0300

    Elementary init solutions

---
+++ Elementary/Correct Sentence/mission.py
@@ -0,0 +1,27 @@
+def correct_sentence(text: str) -> str:
+    """
+        returns a corrected sentence which starts with a capital letter
+        and ends with a dot.
+    """
+    # your code here
+    text_list = list(text)
+    if text_list[0].islower():
+        text_list[0] = text_list[0].upper()
+
+    if text_list[-1] != ".":
+        text_list.append(".")
+    return ''.join(text_list)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(correct_sentence("greetings, friends"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert correct_sentence("greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends.") == "Greetings, friends."
+    assert correct_sentence("hi") == "Hi."
+    assert correct_sentence("welcome to New York") == "Welcome to New York."
+
+    print("Coding complete? Click 'Check' to earn cool rewards!")
